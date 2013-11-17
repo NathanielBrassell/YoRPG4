c9f093133805bd1c15732caa638c7dfe814a78a6
Anya Hargil <anya.hargil@gmail.com>
2013-11-17 18:46:13 -0500

    Update Sage.java

--- Sage.java
+++ Sage.java
@@ -1,2 +1,29 @@
 public class Sage extends Character{
+  
+    String _name = "";
+
+    public Sage(String name){
+
+        _name = name;
+        _hp = 150;
+        _str = 85;
+        _def = 60;
+        _atk = 0.5;
+
+    }
+
+    public String about(){
+        return "A Sage uses his wisdom, knowledge of nature and mystical powers to defeat powers of evil.";
+    }
+    
+    public void specialize(){
+            _def = 40;
+            _atk = 0.8;
+    }
+    
+    public void normalize(){
+            _def = 60;
+            _atk = 0.5;
+    }
+  
 }
