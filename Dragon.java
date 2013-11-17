4cdd64c83800611f335a4eba36c9df1e0415ac00
Nathaniel Brassell <nathaniel303@gmail.com>
2013-11-17 17:15:14 -0500

    Update Dragon.java

--- Dragon.java
+++ Dragon.java
@@ -1,2 +1,18 @@
 public class Dragon extends Character{
+  
+  public Dragon(){
+    
+    _str = 25 + ((int)Math.random() * 55));
+    _hp = 160;
+    _def = 30;
+    _atk = 1.2;
+    
+  }
+  
+  public String about(){
+    String ret = "";
+    ret += "A Dragon is the most feared enemy of all adventures.\n";
+    ret += "They have increased strngth and atack rate, and their scales are harder than any armour.";
+    return ret
+  }
 }
