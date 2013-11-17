c9b31d0bd9b8ab026daa3663313bb18677f0f7ef
Nathaniel Brassell <nathaniel303@gmail.com>
2013-11-17 16:42:54 -0500

    Update Warrior.java

--- Warrior.java
+++ Warrior.java
@@ -8,14 +8,22 @@ public class Warrior extends Character{
 	_hp = 125;
 	_str = 100;
 	_def = 40;
-	permdef = 40;
 	_atk = 0.4;
-	permatk = 0.4;
 
     }
 
     public String about(){
 	return "A Warrior uses his sword to cleave thru his enemy's flesh and bone";
+    }
+    
+    public void specialize(){
+    	_def = 30;
+    	_atk = 0.7;
+    }
+    
+    public void normalize(){
+    	_def = 40;
+    	_atk = 0.4;
     }
 
 }
