eb8e5014e8351901285d029d8a98b1de2bdd9490
Anya Hargil <anya.hargil@gmail.com>
2013-11-17 16:28:24 -0500

    Update Archer.java

--- Archer.java
+++ Archer.java
@@ -1,2 +1,102 @@
-public class Archer extends Character{
-}
+  /*=============================================
+    class Archer -- the second protagonist of Ye Olde RPG
+    =============================================*/
+  
+  public class Archer extends Character {
+  
+      // ~~~~~~~~~~~ INSTANCE VARIABLES ~~~~~~~~~~~
+      private String _name = "Robbin Hood";
+      private int _hitPts, _strength, _defense;
+      private double _attack;
+      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+  
+      /*=============================================
+        default constructor
+        pre:  instance vars are declared
+        post: initializes instance vars.
+        =============================================*/
+      public Archer() {
+  	_hitPts = 125;
+  	_strength = 100;
+  	_defense = 40;
+  	_attack = .4; 
+      }
+  
+  
+      /*=============================================
+        overloaded constructor
+        pre:  instance vars are declared
+        post: initializes instance vars. _name is set to input String.
+        =============================================*/
+      public Archer( String name ) {
+  	this();
+  	_name = name;
+      }
+  
+  
+      // ~~~~~~~~~~~~~~ ACCESSORS ~~~~~~~~~~~~~~~~~
+      public String getName() { return _name; }
+      
+      public int getDefense() { return _defense; }
+      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+      
+      /*=============================================
+        boolean isAlive() -- tell whether I am alive
+        post: returns boolean indicated alive or dead
+        =============================================*/
+      public boolean isAlive() {
+  	return _hitPts > 0;
+      }
+  
+  
+      /*=============================================
+        int attack(Monster) -- simulates attack on instance of class Monster
+        pre:  Input not null
+        post: Calculates damage to be inflicted, flooring at 0. 
+              Calls opponent's lowerHP() method to inflict damage. 
+  	    Returns damage dealt.
+        =============================================*/
+      public int attack( Monster opponent ) {
+  
+  	int damage = (int)( (_strength * _attack) - opponent.getDefense() );
+  	//System.out.println( "\t\t**DIAG** damage: " + damage );
+  
+  	if ( damage < 0 )
+  	    damage = 0;
+  
+  	opponent.lowerHP( damage );
+  
+  	return damage;
+      }//end attack
+  
+  
+      /*=============================================
+        void lowerHP(int) -- lowers life by input value
+        pre:  Input >= 0
+        post: Life instance var is lowered by input ammount.
+        =============================================*/
+      public void lowerHP( int damageInflicted ) {
+  	_hitPts = _hitPts - damageInflicted;
+      }
+  
+  
+      //prepare a Archer for a special attack
+      public void specialize() {
+  	_attack = .75;
+  	_defense = 20;
+      }
+  
+      //revert to normal mode
+      public void normalize() {
+  	_attack = .4;
+  	_defense = 40;
+      }
+  
+      //describes the object
+      public static String about() {
+  	return "You are a brave and courageous archer engaging in long-range combat with the monstes of this world.";
+      }
+  
+  }//end class Archer
