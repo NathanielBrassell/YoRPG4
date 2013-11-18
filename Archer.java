abbfd5a5863d2a88425c5b83a2ae5888b160f771
Anya Hargil <anya.hargil@gmail.com>
2013-11-17 20:21:56 -0500

    Update Archer.java

--- Archer.java
+++ Archer.java
@@ -33,63 +33,7 @@
   	    this();
   	    _name = name;
       }
-  
-  
-      // ~~~~~~~~~~~~~~ ACCESSORS ~~~~~~~~~~~~~~~~~
-      public String getName() { return _name; }
-      
-      public int getDefense() { return _def; }
-      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  
-      
-      /*=============================================
-        boolean isAlive() -- tell whether I am alive
-        post: returns boolean indicated alive or dead
-        =============================================*/
-      public boolean isAlive() {
-  	    return _hp > 0;
-      }
-  
-  
-      /*=============================================
-        int attack(Monster) -- simulates attack on instance of class Monster
-        pre:  Input not null
-        post: Calculates damage to be inflicted, flooring at 0. 
-              Calls opponent's lowerHP() method to inflict damage. 
-  	    Returns damage dealt.
-        =============================================*/
-      public int attack( Monster opponent ) {
-  
-  	    int damage = (int)( (_str * _atk) - opponent.getDefense() );
-  	    //System.out.println( "\t\t**DIAG** damage: " + damage );
-  
-  	    if ( damage < 0 )
-  	      damage = 0;
-  
-  	    opponent.lowerHP( damage );
-  
-  	    return damage;
-      }//end attack
-  
-  
-      /*=============================================
-        void lowerHP(int) -- lowers life by input value
-        pre:  Input >= 0
-        post: Life instance var is lowered by input ammount.
-        =============================================*/
-      public void lowerHP( int damageInflicted ) {
-  	    _hp = _hp - damageInflicted;
-      }
-  
-  
-      public void HPot(){
-        if (_Hpot > 0){
-          _hp = _hp + 25;
-        }
-        else{
-          System.out.println("Thou hast no more Health Potions");
-        }
-      }
+
       
       //prepare a Archer for a special attack
       public void specialize() {
