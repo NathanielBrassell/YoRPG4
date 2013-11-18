52bd5b5174252cf0c14c395db126dc823adaeff2
Anya Hargil <anya.hargil@gmail.com>
2013-11-17 21:10:17 -0500

    Update Cleric.java

--- Cleric.java
+++ Cleric.java
@@ -23,7 +23,7 @@ public class Cleric extends Character{
   }
   
   public String Stats(){
-    String ret;
+    String ret = "";
     ret += _name + " is a Cleric with a defense of "+ _def + ", a strength of "+ _str+ " and has " + _hp+ "Health Points left.";
     return ret;
     }
