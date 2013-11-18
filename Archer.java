f20f5abd5f8359701ee1df38fe97654a6b7d0e8e
Anya Hargil <anya.hargil@gmail.com>
2013-11-17 21:09:31 -0500

    Update Archer.java

--- Archer.java
+++ Archer.java
@@ -49,7 +49,7 @@
       
       //describes the Archers Stats
       public String Stats(){
-        String ret;
+        String ret = "";
         ret += _name + " is an Archer with a defense of "+ _def + ", a strength of "+ _str+ " and has " + _hp+ "Health Points left.";
         return ret;
       }
